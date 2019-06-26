471629d3a19215840ef71508c470a375850158b1
zhousen <441472055@qq.com>
2019-06-26 20:04:37 +0800

    1234

---
+++ src/main/java/com/itheima/web/Servlet2.java
@@ -0,0 +1,13 @@
+package com.itheima.web;
+
+import java.io.IOException;
+
+public class Servlet2 extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
